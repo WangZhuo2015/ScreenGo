914aba8fbc5476da581a49b661d78d62bde13c0a
王卓 <zhuowangwork@gmail.com>
2023-06-16 11:47:00 +0800

    Initial Commit

---
+++ ScreenGo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScreenGo
+//
+//  Created by wangzhuo on 16/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScreenGo/ScreenGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenGoApp.swift
+//  ScreenGo
+//
+//  Created by wangzhuo on 16/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScreenGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
